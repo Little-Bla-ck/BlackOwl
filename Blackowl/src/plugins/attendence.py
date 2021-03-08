6198d3a15ad9aa4d1c9ae7ea3289d890837077cd
Little-Bla-ck <1173198173@qq.com>
2021-03-08 08:04:46 +0800

    attdence

--- Blackowl/src/plugins/attendence.py
+++ Blackowl/src/plugins/attendence.py
@@ -8,32 +8,35 @@ import json
 import math
 
 attendence = on_command('签到')
+sighed = [0,0,0,0,0]
+# 0:id
+# 1:money
+# 2:sigh_days
 
 @attendence.handle()
 async def sigh(bot:Bot, event:GroupMessageEvent, state:T_State):
-    id = int(event.get_user_id())
-    sigh = [0,1,2,3,4]
-    # 0:id
-    # 1:money
-    # 2:sigh_days
-    with open("C:\PyProject\lucky\src\plugins\\group_data.json", "r",encoding = 'utf-8') as f:
+    id = str(event.get_user_id())
+    with open("C:\Coding\Python\kqhttp\\group_data.json", "r",encoding = 'utf-8') as f:
         data = list(json.load(f))
-    for i in range(0,len(data)):
-        if data[i[0]] == id:
-            data[i[2]] = data[i[2]] + 1
-            data[i[1]] = data[i[1]] + money_cul(data[i[2]])
-            msg = f'亲爱的群成员({id}),签到成功(*^▽^*)~\n你已经连续签到{data[i[2]]}天\n今天签到获得硬币{money_cul(data[i[2]])}\n账户内硬币数：{data[i[1]]}'
+    for i in range(0, len(data)):
+        lipu = data[i]
+        if str(lipu[0]) == id:
+            lipu[2] = lipu[2] + 1
+            lipu[1] = lipu[1] + int(money_cul(lipu[2]))
+            msg = f'亲爱的群成员({id}),签到成功(*^▽^*)~\n你已经连续签到{lipu[2]}天\n今天签到获得硬币{int(money_cul(lipu[2]))}\n账户内硬币数：{lipu[1]}'
             await attendence.finish(msg)
     
-    sigh[0] = id
-    sigh[1] = 0
-    sigh[2] = 1
-    data.append(sigh)
-    msg = f'亲爱的群成员({id}),签到成功(*^▽^*)~\n你已经连续签到{sigh[2]}天\n今天签到获得硬币{money_cul(sigh[2])}\n账户内硬币数：{sigh[1]}'
+    sighed[0] = id
+    sighed[2] = 1
+    sighed[1] = int(money_cul(sighed[2]))
+    
+    data.append(sighed)
+    msg = f'亲爱的群成员({id}),签到成功(*^▽^*)~\n你已经连续签到{sighed[2]}天\n今天签到获得硬币{int(money_cul(sighed[2]))}\n账户内硬币数：{sighed[1]}'
+    with open("C:\Coding\Python\kqhttp\\group_data.json", "w",encoding = 'utf-8') as f:
+        f.write(json.dumps(data))
     await attendence.finish(msg)
 
-    with open("C:\PyProject\lucky\src\plugins\\group_data.json", "w",encoding = 'utf-8') as f:
-        f.write(json.dumps(data))
+    
 
 
 
@@ -42,5 +45,5 @@ def money_cul(days):
     if days == 1:
         return 1;
     days = float(days)
-    money = 10 * log(days * 0.06 + 1)
+    money = 10 * math.log(days * 0.06 + 1)
     return int(money)
